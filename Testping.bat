854130bcc13805c38214d9d7532646645770ce1e
ZockerKatze <89251490+ZockerKatze@users.noreply.github.com>
2021-08-20 12:11:21 +0200

    Add files via upload

---
+++ Testping.bat
@@ -0,0 +1,18 @@
+cls
+color 04
+echo off
+title PINGTOOL V.2 dev version 
+cls
+color 04
+echo The MS is in the middel means the delay! EXAMPLE= "TIME=12ms"!
+echo These are not the google servers that are pinged!
+:wwwa
+color 04
+echo Ping succsesfully!
+color 04
+ping 1.1.1.1
+goto wwwa
+
+
+//you can exit it by hitting the X
+trolloistcoollogoistschwul
