e324ab268f24b1f420640cbb38503f4106e3e0cc
ZockerKatze <89251490+ZockerKatze@users.noreply.github.com>
2021-08-20 13:45:49 +0200

    Add files via upload
    -added google testping programm
    -added info.txt file

---
+++ Tesping_google.bat
@@ -0,0 +1,18 @@
+echo google version
+color 04
+echo off
+title PINGTOOL V.2 dev version 
+cls
+color 04
+echo The MS is in the middel means the delay! EXAMPLE= "TIME=12ms"!
+echo These are the google servers that are pinged!
+:wwwa
+color 04
+echo Ping succsesfully!
+color 04
+ping 172.217.19.110
+goto wwwa
+
+
+//you can exit it by hitting the X
+trolloistcoollogoistschwul
